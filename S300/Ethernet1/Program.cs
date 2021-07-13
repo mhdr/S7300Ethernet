4bd8ca727617557fc90c9634bfbb1ab806ade6f1
Mahmood Ramzani <mahmoodramzani@gmail.com>
2021-07-13 18:52:59 +0430

    project created

---
+++ S300/Ethernet1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sharp7;
+
+namespace Ethernet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S7Client client = new S7Client();
+            int result = client.ConnectTo("127.0.0.1", 0, 2);
+
+            if (result != 0)
+            {
+                Console.WriteLine(client.ErrorText(result));
+            }
+        }
+    }
+}
